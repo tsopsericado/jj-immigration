3f0edda5178380d9b1a51ead8720401f0bc4477e
ricardo <ricardotsopse@gmail.com>
2024-06-21 04:56:56 +0100

    link have done

---
+++ src/app/components/Navbar/MyLinks.tsx
@@ -0,0 +1,114 @@
+export const links = [
+  {
+    name: "TRAVAILLER",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: "EMIT", link: "/" },
+          { name: "PERMIS DE TRAVAIL", link: "/" },
+          { name: "PROLONGATION DE PERMIS DE TRAVAIL", link: "/" },
+        ],
+      },
+    ],
+  },
+
+
+
+  {
+    name: "IMMIGRER",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: "ENTREES EXPRESS - TRAVAILLEURS QUALIFIES FEDERALE(TQF)", link: "/" },
+          { name: "ENTREES EXPRESS - CATEGOGIE DE L'EXPERIENCE CANADIENNE (CEC)", link: "/" },
+          { name: "PROGRAMMES DU QUEBEC (PEQ)", link: "/" },
+          { name: "PROGRAMMES DU QUEBEC (PEQ)", link: "/" },
+          { name: "PROGRAMMES DES CANDIDATS DES PROVINCES (PCP)", link: "/" },
+          { name: "PARRAINER VOTRE EPOUX, CONJOINT DE FAIT OU ENFANT A CHARGE ", link: "/" },
+          { name: "PARRAINER VOS PARENTS OU VOS GRAND-PARENTS", link: "/" },
+        ],
+      },
+    ],
+  },
+
+
+
+  {
+    name: "VISITER",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: "VISA VISITEUR AU CANADA", link: "/" },
+          { name: "SUPERVISA POUR LES PARENTS ET LES GRAND-PARENTS", link: "/" },
+        ],
+      },
+    ],
+  },
+
+
+
+
+  {
+    name: "Kid's",
+    submenu: true,
+    sublinks: [
+      {
+        Head: "Topwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+        ],
+      },
+      {
+        Head: "Bottomwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+        ],
+      },
+      {
+        Head: "innerwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+        ],
+      },
+
+      {
+        Head: "sleepwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+        ],
+      },
+      {
+        Head: "footwear",
+        sublink: [
+          { name: "T-shirt", link: "/" },
+          { name: "Casual shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+          { name: "formal shirts", link: "/" },
+        ],
+      },
+    ],
+  },
+];
