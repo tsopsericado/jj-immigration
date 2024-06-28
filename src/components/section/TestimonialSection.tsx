f75c5009c6e32ea6c19ae403fcfb399edc4baf08
GaelTikeng <Gaelinho@yahoo.fr>
2024-06-28 13:16:11 +0100

    Done with the responsiveness of the landing page

--- src/components/section/TestimonialSection.tsx
+++ src/components/section/TestimonialSection.tsx
@@ -127,54 +127,6 @@ function TestimonialSection() {
         </Carousel>
       </div>
     </section>
-
-    // <section className="flex justify-between bg-green-300 flex-col xl:flex-row items-center lg:-mt-[5rem] gap-16 pt-32 lg:mb-20 sm:mb-8">
-    //   <div>
-    //     <p className="text-lightGray text-2xl font-[600] text-left uppercase">
-    //       Témoignages
-    //     </p>
-    //     <p className="volkhov text-5xl text-title  text-left">
-    //       Ce que nos clients disent de nous</p>
-
-    //     <div className="mt-[5.12rem] ">
-    //       <img
-    //         src="/assets/images/slide-indicator.png"
-    //         alt="slide indicator"
-    //         className="hidden md:block"
-    //       />
-    //     </div>
-    //   </div>
-    //   <div className="flex items-center gap-4 md:gap-[4.12rem] ">
-    //     <div className="relative">
-    //       {/* {reviews.map((rev, indx) => ())} */}
-    //       <TestimonialCard
-    //         key={reviews[0].id}
-    //         position={reviews[0].position}
-    //         review={reviews[0].review}
-    //         reviewerName={reviews[0].reviewerName}
-    //         imageUrl={reviews[0].imageUrl}
-    //       />
-    //       <div className="absolute -bottom-[6rem] left-32 -z-10">
-    //         <TestimonialCard
-    //           key={reviews[1].id}
-    //           position={reviews[1].position}
-    //           review={reviews[1].review}
-    //           reviewerName={reviews[1].reviewerName}
-    //           imageUrl={reviews[1].imageUrl}
-    //           isBackdrop
-    //         />
-    //       </div>
-    //     </div>
-    //     <div className="flex flex-col gap-16">
-    //       <div className="hover:cursor-pointer">
-    //         <img src="/assets/images/chevron-up.png" alt="chevron up" />
-    //       </div>
-    //       <div className="hover:cursor-pointer">
-    //         <img src="/assets/images/chevron-down.png" alt="chevron up" />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
   );
 }
 
