d4a08216c77cc9f854cb4014494fc37e426881bb
GaelTikeng <Gaelinho@yahoo.fr>
2024-07-19 06:47:50 +0100

    slight change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.min.css";
 
 const fontSans = FontSans({
   subsets: ["latin"],
-  variable: "--font-sans",
+  variable: "--font-sans"
 });
 
 export const metadata: Metadata = {
